7ab824b0dacb910999b6a929262f64a93c9d81c1
KoushikKapaka <126226144+KoushikKapaka@users.noreply.github.com>
2024-01-22 21:26:45 +0530

    Xanov4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 export default function Home() {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense>
         <Form></Form>
       </Suspense>
     </>
