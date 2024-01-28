514ba1be6921c37333b215cd9f73687ec394260c
KoushikKapaka <126226144+KoushikKapaka@users.noreply.github.com>
2024-01-29 00:05:13 +0530

    improved

--- src/app/razorpay/page.tsx
+++ src/app/razorpay/page.tsx
@@ -88,7 +88,7 @@ const Home = () => {
           life: 3000,
         });
         router.push(
-          "https://auth.razorpay.com/authorize?response_type=code&client_id=NSxAbZB40wuIsx&redirect_uri=https://inspiring-brigadeiros-5fce73.netlify.app/razorpay/success/&scope=read_only&state=${state}"
+          `"https://auth.razorpay.com/authorize?response_type=code&client_id=NSxAbZB40wuIsx&redirect_uri=https://inspiring-brigadeiros-5fce73.netlify.app/razorpay/success/&scope=read_only&state=" ${state}`
         );
       }
     } else {
