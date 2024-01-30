38c81e9c0ad7ae2aa2f6756a5eef98f3ba270b7b
KoushikKapaka <126226144+KoushikKapaka@users.noreply.github.com>
2024-01-30 09:15:47 +0530

    changed the scope of razorpay

--- src/app/razorpay/page.tsx
+++ src/app/razorpay/page.tsx
@@ -78,7 +78,7 @@ const Home = () => {
           life: 3000,
         });
         router.push(
-          `https://auth.razorpay.com/authorize?response_type=code&client_id=NUSKb4cAK4iP9e&redirect_uri=https://inspiring-brigadeiros-5fce73.netlify.app/razorpay/success/&scope=read_only&state=${state}`
+          `https://auth.razorpay.com/authorize?response_type=code&client_id=NUSKb4cAK4iP9e&redirect_uri=https://inspiring-brigadeiros-5fce73.netlify.app/razorpay/success/&scope=read_write&state=${state}`
         );
       }
     } else {
